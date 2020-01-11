672fc12a85620a60ed1907c821a9f33170dde06f
hepengyuan <hpy253215039@163.com>
2020-01-12 01:00:19 +0800

    add

--- istio-1.4.2/simple-flask-app/main.py
+++ istio-1.4.2/simple-flask-app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 import os
-import urllib.request
+from urllib.request import Request, urlopen
+import json
 
 app = Flask(__name__)
 
@@ -13,6 +14,41 @@ def fetch_env():
     url = request.args.get('url', '')
     with urllib.request.urlopen(url) as response:
         return response.read()
+
+TRACE_HEADERS = [
+    'x-request-id',
+    'x-b3-parentspanid',
+    'x-b3-sampled',
+    'x-b3-spanid',
+    'x-b3-traceid',
+    'x-b3-flags',
+    'x-ot-span-context'
+]
+
+@app.route('/fetch_with_header')
+def fetch_with_headers():
+    url = request.args.get('url', '')
+    request_headers = dict(request.headers)
+    request_headers = dict(request.headers)
+    new_header = {}
+    for key in request_headers.keys():
+        if key.lower() in TRACE_HEADERS:
+            new_header[key] = request_headers[key]
+
+    req = Request(url, headers = new_header)
+    res = urlopen(req).read()
+    return  "{}\n{}".format(json.dumps(request_headers), res)
+
+@app.route('/fetch_with_trace')
+def fetch_with_trace():
+    url = request.args.get('url', '')
+    request_headers = dict(request.headers)
+    new_header = {}
+    for key in request_headers.keys():
+        if key.lower() in TRACE_HEADERS:
+            new_header[key] = request_headers[key]
+    req = Request(url, headers = new_header)
+    return urlopen(req).read()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7777, debug=True)
